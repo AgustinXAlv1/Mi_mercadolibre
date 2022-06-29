c6889e2cc804226bfe76e87bf04271d046547b84
AgustinXAlv1 <alvarezagustinx@gmail.com>
2022-06-28 21:04:02 -0300

    Agrego register

--- app.js
+++ app.js
@@ -11,7 +11,14 @@ app.listen(3000, ()=>{
 })
 
 app.get('/', (req,res)=>{
-    res.sendFile(path.join(__dirname,"/views/home.html"))
-    
+    res.sendFile(path.join(__dirname,"/views/home.html"));
+})
+
+app.get('/register', (req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/register.html"));
+})
+
+app.get('/home', (req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/home.html"));
 })
 
